9ce1fa3a6c2efb40b22643e006eaae5ae56bf72c
Jan Lipnican <jan@lipnican.dev>
2021-06-12 22:16:43 +0200

    Feature: notify emails on new products

---
+++ add_subscriber.js
@@ -0,0 +1,30 @@
+import * as db from "./lib/db.js";
+
+function isParam(param) {
+    return process.argv.indexOf(param) !== -1;
+}
+
+function getParamValue(param) {
+    if (!isParam(param)) {
+        return null;
+    }
+
+    return process.argv[process.argv.indexOf(param) + 1];
+}
+
+async function main() {
+    console.log("🌟 Running add subscriber.");
+
+    const email = getParamValue("--email");
+    const name = getParamValue("--name");
+
+    console.log(`➕ Adding ${name} with email ${email}.`);
+    
+    await db.addSubscriber({ email, name });
+
+    console.log("✅ Subscriber added.")
+
+    process.exit(0);
+}
+
+main();
---
+++ check.js
@@ -0,0 +1,52 @@
+import * as Ciernediery from "./lib/ciernediery.js";
+import * as Email from "./lib/email.js";
+import * as db from "./lib/db.js";
+import ejs from "ejs";
+
+function handleError(error) {
+    console.error(`❌ Check failed. Cause: ${error}.`);
+    process.exit(1);
+}
+
+async function sendNotifications(newProducts) {
+    console.log("🛩️ Sending notifications...");
+
+    const subscribers = await db.getSubscribers();
+
+    if (subscribers.length === 0) {
+        console.log("🤷 No subscribers.");
+        return;
+    }
+
+    await Promise.all(subscribers.map(async ({ email, name }) => {
+        const body = await ejs.renderFile("./email.template.ejs", { email, name, newProducts });
+        await Email.sendMail(email, "New products at ciernediery.sk", body);
+    }));
+
+    console.log("✅ Notifications were sent.");
+}
+
+async function main() {
+    console.log("🌟 Running check.");
+
+    try {
+        const newProducts = await Ciernediery.getNewProducts();
+
+        if (newProducts.length > 0) {
+            console.log(`😃 Found ${newProducts.length} new ${newProducts.length > 1 ? "products" : "product"}.`);
+
+            await sendNotifications(newProducts);
+        } else {
+            console.log("😥 No new products detected.");
+        }
+
+        console.log("✅ Check successful.")
+    } catch(err) {
+        handleError(err);
+        process.exit(1);
+    }
+
+    process.exit(0);
+}
+
+main();
