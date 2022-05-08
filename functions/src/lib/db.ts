fea05c4ed1d4f539fe855d2d24d65d3ff361fc6e
Jan Lipnican <jan.lipnican@productboard.com>
2022-05-08 21:36:00 +0200

    db cleanup

--- functions/src/lib/db.ts
+++ functions/src/lib/db.ts
@@ -2,7 +2,6 @@ import {Product} from "./ciernediery";
 import {getFirestore} from "firebase-admin/firestore";
 
 const PRODUCTS_KEY = "products";
-const SUBSCRIBERS_KEY = "subscribers";
 
 export type Subscriber = {
     name: string;
@@ -15,12 +14,6 @@ export async function setProducts(products: Array<Product>) {
 
 export async function getProducts(): Promise<Array<Product>> {
   const doc = await getFirestore().collection(PRODUCTS_KEY).doc(PRODUCTS_KEY).get();
-  const {products} = await doc.data() as { products: Array<Product> };
+  const products = await doc.get(PRODUCTS_KEY) as Array<Product>;
   return products;
-}
-
-export async function getSubscribers(): Promise<Array<Subscriber>> {
-  const doc = await getFirestore().collection(SUBSCRIBERS_KEY).doc(SUBSCRIBERS_KEY).get();
-  const {subscribers} = await doc.data() as { subscribers: Array<Subscriber> };
-  return subscribers;
 }
